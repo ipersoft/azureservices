6578a49b2f10d96c176e0b469188bd2bb3fea3c6
Paolo Fisco <paolo.fisco@gmail.com>
2016-09-19 15:59:16 +0200

    Create meteo.php

---
+++ infocitta/meteo.php
@@ -0,0 +1,35 @@
+<?php
+  include 'token.php';
+  $meteo = file_get_contents("http://api.wunderground.com/api/".$tokenwg."/conditions/lang:IT/q/Italy/".$_SERVER['city'].".json");
+  $json = json_decode($meteo, true);
+  $condizione = $json['current_observation']['weather'];
+  $temp= $json['current_observation']['temp_c'];
+  $umidita=$json['current_observation']['relative_humidity'];
+  $icon=$json['current_observation']['icon'];
+  
+
+  switch ($icon) {
+    case "clear":
+      $icont=":sunny:";
+      break;
+    case "rain":
+      $icont=":umbrella:";
+      break;
+    case "partlycloudy":
+      $icont=":partly_sunny:";
+      break;
+    case "mostlycloudy":
+      $icont=":partly_sunny:";
+      break;
+    case "cloudy":
+      $icont=":cloud:";
+      break;
+    case "sunny":
+      $icont=":sunny:";
+      break;      
+  }
+  
+  echo 'Condizioni: '.$condizione." ".$icont."\r\n";
+  echo 'Temperatura: '.$temp." C\r\n";
+  echo 'Umidita\': '.$umidita."\r\n";
+?>
